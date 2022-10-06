f37b86672a0ff27ade4958d766c5a156f643f863
jasonkiptoo <kiptoojason@gmail.com>
2022-10-06 23:24:47 +0300

    source

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import HomePage from "./HomePage";
+
+function App({ handleAdd, employee }) {
+  return (
+    <div className="App">
+     <HomePage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HomePage.js
@@ -0,0 +1,52 @@
+import React, { useEffect } from "react";
+import Home from "./Home";
+import AddNewEmployee from "./AddNewEmployee";
+import EmployeeList from "./EmployeeList";
+import NavBar from "./NavBar";
+import { useState } from "react";
+import SalaryPays from "./SalaryPays";
+import PendingSalaries from "./PendingSalaries";
+import { Routes, Route } from "react-router-dom";
+
+function HomePage() {
+  const [employee, setEmployee] = useState([]);
+
+  const handleAdd = (employeeData) => {
+    fetch("https://emp-db.herokuapp.com/users", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(employeeData),
+    });
+  };
+
+  function userList() {
+    fetch("https://emp-db.herokuapp.com/users")
+      .then((res) => res.json())
+      .then((data) => setEmployee(data));
+  }
+  useEffect(() => {
+    userList();
+  }, []);
+  return (
+    <div>
+      <NavBar />
+
+      <Routes>
+        <Route exact path="/" element={<Home empl={employee} />} />
+        <Route
+          exact
+          path="/addnewemployee"
+          element={<AddNewEmployee onSubmit={handleAdd} />}
+        />
+        <Route
+          exact
+          path="/employeelist"
+          element={<EmployeeList userList={employee} />}
+        />
+        <Route exact path="/salarypayments" element={<SalaryPays />} />
+        <Route exact path="/pendingsalaries" element={<PendingSalaries />} />
+      </Routes>
+    </div>
+  );
+}
+export default HomePage;
